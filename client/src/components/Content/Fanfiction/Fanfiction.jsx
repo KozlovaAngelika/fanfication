7bacd83a03db5dd7b2d0b779c4f83216f64f7065
KozlovaAngelika <kozlovaangelika09@gmail.com>
2021-07-09 16:29:26 +0300

    feat: add button for sort

--- client/src/components/Content/Fanfiction/Fanfiction.jsx
+++ client/src/components/Content/Fanfiction/Fanfiction.jsx
@@ -10,8 +10,12 @@ const Fanfiction = ({ fanfictionData }) => {
         return <Fanfic data={elem} key={elem._id}></Fanfic>
     })
     return (
-        <Row className={`${style.fanfiction}`}>
+        <Row className={style.fanfiction}>
             <Col>
+                <Row className='pb-3'>
+                    <Col xs={6}><h1>All fanfiction</h1></Col>
+                    <Col xs={6} className='d-flex flex-column justify-content-center align-items-end'><div className={style.sortByDateBtn}>Sort by date<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sort" class="svg-inline--fa fa-sort fa-w-10" role="img" viewBox="0 0 320 512"><path fill="currentColor" d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z" /></svg></div></Col>
+                </Row>
                 {FanfictionElements}
             </Col>
         </Row >
