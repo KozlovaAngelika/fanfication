98e7d06d2d5b1287310776dd693c403636bd16dc
KozlovaAngelika <kozlovaangelika09@gmail.com>
2021-07-09 16:00:13 +0300

    fix: replace date format function to front

--- routes/fanfiction.routes.js
+++ routes/fanfiction.routes.js
@@ -2,21 +2,11 @@ import Router from 'express';
 import Fanfic from './../models/fanfication.js'
 
 const funfictionRouter = Router();
-const formatDate = (date) => {
-    let dd = date.getDate();
-    if (dd < 10) dd = '0' + dd;
 
-    let mm = date.getMonth() + 1;
-    if (mm < 10) mm = '0' + mm;
-
-    let yy = date.getFullYear() % 100;
-    if (yy < 10) yy = '0' + yy;
-    return `${dd}.${mm}.${yy}`;
-}
 funfictionRouter.post('/fanfiction', async (req, res) => {
     try {
         const { name, fandom, content, owner } = req.body;
-        const lastUpdateDate = formatDate(new Date());
+        const lastUpdateDate = new Date();
         const fanfic = await new Fanfic({
             name: name,
             fandom: fandom,
