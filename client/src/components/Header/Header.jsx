b3012435588625944dd4cf5b7c8733a2aa14ce53
KozlovaAngelika <kozlovaangelika09@gmail.com>
2021-07-10 01:13:15 +0300

    feat: add handler for logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,12 +5,17 @@ import AuthLinks from './AuthLinks/AuthLinks';
 import ThemeSwitcherBtn from './ThemeSwitcherBtn/ThemeSwitcherBtn';
 import style from './Header.module.scss';
 import logo from '@media/logo.jpg'
+import { useHistory } from "react-router-dom";
 const Header = ({ currentTheme, setCurrentTheme }) => {
+    const history = useHistory();
+    const logoHandler = () => {
+        history.push("/");
+    }
     return (
         <Container className={style.header} fluid>
             <Row className={style.navigation}>
                 <Col sm={12} md={6} className='d-flex justify-content-between align-items-center'>
-                    <img src={logo} alt="logo" className={style.logo} />
+                    <img src={logo} alt="logo" className={style.logo} onClick={logoHandler} />
                 </Col>
                 <Col sm={12} md={3} className='d-flex justify-content-md-end  align-items-center'>
                     <AuthLinks></AuthLinks>
