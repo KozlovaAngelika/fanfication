45a1d3589642db7519aee0ccef4608adcdbc9675
KozlovaAngelika <kozlovaangelika09@gmail.com>
2021-07-09 18:40:47 +0300

    feat: add loader to main page

--- client/src/components/Content/Main/Main.jsx
+++ client/src/components/Content/Main/Main.jsx
@@ -4,13 +4,18 @@ import { Col } from 'react-bootstrap';
 import style from './Main.module.scss';
 import Funfiction from './../Fanfiction/Fanfiction';
 import Loader from '../../Loader/Loader'
+import ErrorMessage from '../../ErrorMessage/ErrorMessage'
 import axios from 'axios';
 
 const Main = () => {
     const [allFanfiction, setAllFanfiction] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
     const getFanfiction = useCallback(
         async () => {
+            setIsLoading(true)
             const data = await axios.get("/api/funfic/fanfiction").then((res) => {
+                setIsLoading(false);
                 return res.data;
             });
             setAllFanfiction(data);
@@ -18,12 +23,16 @@ const Main = () => {
     useEffect(
         getFanfiction, [getFanfiction]
     )
+    const loaderElement = isLoading ? <Loader></Loader> : null;
+    const errorElement = error ? <ErrorMessage></ErrorMessage> : null;
+    const contentElement = !isLoading && !error ? <Funfiction fanfictionData={allFanfiction ? allFanfiction : []}></Funfiction> : null;
     return (
         <Col className={style.main}>
             < SearchPanel ></SearchPanel >
             <div>
-                {/* <Funfiction fanfictionData={allFanfiction ? allFanfiction : []}></Funfiction> */}
-                < Loader ></Loader >
+                {loaderElement}
+                {errorElement}
+                {contentElement}
             </div>
 
         </Col>
---
+++ client/src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorMessage = () => {
+    return (
+        <div>
+            <p>Error</p>
+        </div>
+    );
+}
+
+export default ErrorMessage;
