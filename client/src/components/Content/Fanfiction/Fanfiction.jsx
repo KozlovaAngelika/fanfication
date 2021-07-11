8c4c72ea4dc804cba841dd49496c4ee3b87cace1
KozlovaAngelika <kozlovaangelika09@gmail.com>
2021-07-11 20:20:06 +0300

    refactor: refactor code

--- client/src/components/Content/Fanfiction/Fanfiction.jsx
+++ client/src/components/Content/Fanfiction/Fanfiction.jsx
@@ -8,7 +8,6 @@ const Fanfiction = ({ fanfictionData }) => {
         setIsLastDataFirst((isLastDataFirst) => {
             return !isLastDataFirst;
         });
-        console.log(isLastDataFirst);
     }
     if (fanfictionData.length === 0) {
         return <p className={style.error}>No data to display...</p>
@@ -26,8 +25,8 @@ const Fanfiction = ({ fanfictionData }) => {
         <Row className={style.fanfiction}>
             <Col>
                 <Row className='pb-3'>
-                    <Col xs={6}><h1>All fanfiction</h1></Col>
-                    <Col xs={6} className='d-flex flex-column justify-content-center align-items-end'><div className={style.sortByDateBtn} onClick={sortBtnHandler}>Sort by date<svg viewBox="0 0 448 512"><path d="M413.1 222.5l22.2 22.2c9.4 9.4 9.4 24.6 0 33.9L241 473c-9.4 9.4-24.6 9.4-33.9 0L12.7 278.6c-9.4-9.4-9.4-24.6 0-33.9l22.2-22.2c9.5-9.5 25-9.3 34.3.4L184 343.4V56c0-13.3 10.7-24 24-24h32c13.3 0 24 10.7 24 24v287.4l114.8-120.5c9.3-9.8 24.8-10 34.3-.4z" /></svg></div></Col>
+                    <Col xs={12} md={6}><h1>All fanfiction</h1></Col>
+                    <Col xs={12} md={6} className='d-flex flex-column justify-content-center align-items-md-end'><div className={style.sortByDateBtn} onClick={sortBtnHandler}>Sort by date<svg viewBox="0 0 448 512" className={isLastDataFirst ? style.active : null}><path d="M413.1 222.5l22.2 22.2c9.4 9.4 9.4 24.6 0 33.9L241 473c-9.4 9.4-24.6 9.4-33.9 0L12.7 278.6c-9.4-9.4-9.4-24.6 0-33.9l22.2-22.2c9.5-9.5 25-9.3 34.3.4L184 343.4V56c0-13.3 10.7-24 24-24h32c13.3 0 24 10.7 24 24v287.4l114.8-120.5c9.3-9.8 24.8-10 34.3-.4z" /></svg></div></Col>
                 </Row>
                 {fanfictionElements}
             </Col>
